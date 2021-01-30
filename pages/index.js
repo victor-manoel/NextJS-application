488bccec4851d7a753171e78c083a8dbe9c6bd4a
Victor Manoel <victormanoel7895123@hotmail.com>
2021-01-29 23:56:00 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
+const Title = styled.h4`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
 export default function Home() {
-  return <Title>Hello World</Title>
+  return <Title>projeto</Title>
 }
